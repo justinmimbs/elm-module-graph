618d56a6023a245e3ff21bb699073cb9f0501add
Justin Mimbs <justin.mimbs@gmail.com>
2017-10-09 21:14:22 -0400

    Update elm-module-graph.py to properly namespace module names by package (fix #3), and respect provided filename for Main when it is not named 'Main.elm' (fix #2)

--- elm-module-graph.py
+++ elm-module-graph.py
@@ -12,7 +12,7 @@ re_import = re.compile(r"{-.*?-}|^import\s+([A-Z][\w\.]*)", re.DOTALL | re.MULTI
 
 # source text -> [modulename]
 def extract_imports(text):
-    return filter(lambda s: s != "", re_import.findall(text))
+    return filter(lambda s: s != "" and s[0:7] != "Native.", re_import.findall(text))
 
 
 # source text -> modulename
@@ -38,72 +38,63 @@ def find_elmpackagejson(path):
         return find_elmpackagejson(os.path.dirname(path))
 
 
-# -> [(sourcedir, packagename)]
-def list_sourcedirs_package(packagedir, maybepackagename):
-    packagedata = json.load(open(os.path.join(packagedir, "elm-package.json")))
-    packagename = maybepackagename or extract_packagename(packagedata.get("repository", "")) or "user/project"
-    return map(
-        lambda dir: (os.path.normpath(os.path.join(packagedir, dir)), packagename),
-        packagedata.get("source-directories", [])
-    )
+# -> {"sourcedirs": [dir], "dependencies": [packagename]}
+def get_packageinfo(packagedir):
+    elmpackagedata = json.load(open(os.path.join(packagedir, "elm-package.json")))
+    return {
+        "sourcedirs": map(
+            lambda dir: os.path.normpath(os.path.join(packagedir, dir)),
+            elmpackagedata.get("source-directories", [])
+        ),
+        "dependencies": elmpackagedata.get("dependencies", {}).keys()
+    }
 
 
-# -> [(sourcedir, packagename)]
-def list_sourcedirs(packagedir):
-    sourcedirs = list_sourcedirs_package(packagedir, None)
-    exactdependencies = json.load(open(os.path.join(packagedir, "elm-stuff", "exact-dependencies.json")))
-    dependencysourcedirs = concatmap(
-        lambda (packagename, version): list_sourcedirs_package(os.path.join(packagedir, "elm-stuff", "packages", packagename, version), packagename),
-        exactdependencies.items()
-    )
-    return sourcedirs + dependencysourcedirs
+# -> (packagename, modulename, sourcepath)
+def find_importedmodule(packages, packagename, importedmodulename):
+    sourcedirs = map(tuple(packagename), packages[packagename]["sourcedirs"])
+    dependencysourcedirs = concatmap(lambda dep: map(tuple(dep), packages[dep]["sourcedirs"]), packages[packagename]["dependencies"])
+
+    segments = importedmodulename.split(".")
+    for (importedpackagename, sourcedir) in (sourcedirs + dependencysourcedirs):
+        sourcepath = os.path.join(sourcedir, *segments) + ".elm"
+        if os.path.isfile(sourcepath):
+            return (importedpackagename, importedmodulename, sourcepath)
+
+    print "warning: source file not found for module (" + importedmodulename +") as imported from (" + packagename + ")"
+    return None
+
+
+def tuple(a):
+    return lambda b: (a, b)
 
 
 def concatmap(f, list):
     return reduce(lambda r, x: r + f(x), list, [])
 
 
-# -> (sourcefile, packagename)
-def find_sourcefile(sourcedirs, modulename):
-    segments = modulename.split(".")
-    for sourcedir, packagename in sourcedirs:
-        sourcefile = os.path.join(sourcedir, *segments) + ".elm"
-        if os.path.isfile(sourcefile):
-            return (sourcefile, packagename)
-    return (None, None)
-
-
-# -> graph = {modulename: {"imports": [modulename], "package": string}
-def imports_from_module(sourcedirs, modulename, graph):
-    (sourcefile, packagename) = find_sourcefile(sourcedirs, modulename)
-    if sourcefile is None:
-        print "warning: source file not found for module name: " + modulename
-        return graph
-
-    imports = filter(
-        lambda m: m[0:7] != "Native.",
-        extract_imports(open(sourcefile).read())
+# -> {qualifiedmodulename: {"imports": [qualifiedmodulename], "package": packagename}}
+def graph_from_imports(packages, packagename, modulename, importedmodulenames, graph):
+    importedmodules = filter(
+        lambda x: x is not None,
+        map(lambda m: find_importedmodule(packages, packagename, m), importedmodulenames)
     )
-    graph[modulename] = {"imports": imports, "package": packagename}
-    nextmodules = filter(lambda m: m not in graph, imports)
-    return imports_from_modules(sourcedirs, nextmodules, graph)
+    graph[qualify(packagename, modulename)] = {
+        "imports": map(lambda (p, m, _): qualify(p, m), importedmodules),
+        "package": packagename
+    }
+    return reduce(
+        lambda g, (p, m, sourcepath): graph_from_imports(packages, p, m, extract_imports(open(sourcepath).read()), g),
+        filter(
+            lambda (p, m, _): qualify(p, m) not in graph,
+            importedmodules
+        ),
+        graph
+    )
 
 
-def imports_from_modules(sourcedirs, modulenames, graph):
-    return reduce(lambda g, m: imports_from_module(sourcedirs, m, g), modulenames, graph)
-
-
-# filepath -> (elmpackagejson, [modulename])
-def info_from_filepath(filepath):
-    if os.path.basename(filepath) == "elm-package.json":
-        # elm-package.json
-        modulenames = json.load(open(filepath)).get("exposed-modules", [])
-        return (filepath, modulenames)
-    else:
-        # <Module>.elm
-        elmpackagejson = find_elmpackagejson(filepath)
-        modulename = extract_modulename(open(filepath).read()) or "Main"
-        return (elmpackagejson, [modulename])
+def qualify(packagename, modulename):
+    return packagename + " " + modulename
 
 
 def main():
@@ -118,20 +109,36 @@ def main():
         print "error: file not found: " + filepath
         sys.exit(1)
 
-    (elmpackagejson, modulenames) = info_from_filepath(filepath)
+    elmpackagejson = filepath if os.path.basename(filepath) == "elm-package.json" else find_elmpackagejson(filepath)
 
     if elmpackagejson is None:
         print "error: elm-package.json not found for: " + filepath
         sys.exit(1)
 
-    packagedir = os.path.dirname(elmpackagejson)
+    projectdir = os.path.dirname(elmpackagejson)
 
-    if not os.path.exists(os.path.join(packagedir, "elm-stuff")):
+    if not os.path.exists(os.path.join(projectdir, "elm-stuff")):
         print "error: elm-stuff folder not found (run elm-make and try again)"
         sys.exit(1)
 
-    sourcedirs = list_sourcedirs(packagedir)
-    modulegraph = imports_from_modules(sourcedirs, modulenames, {})
+    # packages (dict), projectname, entryname, modulenames (list)
+    elmpackagedata = json.load(open(elmpackagejson))
+    projectname = extract_packagename(elmpackagedata.get("repository", "")) or "user/project"
+
+    packages = {projectname: get_packageinfo(projectdir)}
+    for (packagename, version) in json.load(open(os.path.join(projectdir, "elm-stuff", "exact-dependencies.json"))).items():
+        packages[packagename] = get_packageinfo(os.path.join(projectdir, "elm-stuff", "packages", packagename, version))
+
+    if filepath == elmpackagejson:
+        entryname = "exposed-modules"
+        modulenames = elmpackagedata.get("exposed-modules", [])
+    else:
+        sourcetext = open(filepath).read()
+        entryname = extract_modulename(sourcetext) or "Main"
+        modulenames = extract_imports(sourcetext)
+
+    # graph
+    modulegraph = graph_from_imports(packages, projectname, entryname, modulenames, {})
 
     output = io.open(args.output, "w", encoding="utf-8")
     output.write(unicode(json.dumps(modulegraph, indent=2, separators=[",", ": "]), encoding="utf-8-sig"))
